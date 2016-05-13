c4bd7378fdd736e814e81e1bbc78b8382fb50736
singularity0 <mr.mihail.petrov@gmail.com>
2016-05-13 17:55:13 +0300

    first 4 tasks for methods

---
+++ 03. Methods/homework/04. Appearance count/AppearanceCount.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace _04.AppearanceCount
+{
+    class AppearanceCount
+    {
+        static void Main(string[] args)
+        {
+            ArrearanceLogic();
+        }
+
+        static void ArrearanceLogic()
+        {
+            string size = Console.ReadLine();
+            string array_entered = Console.ReadLine();
+            string number = Console.ReadLine();
+            int counter = Regex.Matches(array_entered, number).Count;
+            Console.WriteLine(counter);
+        }
+            
+        
+    }
+}
