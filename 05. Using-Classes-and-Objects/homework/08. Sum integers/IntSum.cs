2f31a72fd0d675017e4404bdcfcf8bd9cfd3dcb4
singularity0 <mr.mihail.petrov@gmail.com>
2016-05-20 15:23:24 +0300

    most task resolved

---
+++ 05. Using-Classes-and-Objects/homework/08. Sum integers/IntSum.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08.SumIntegers
+{
+    class IntSum
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            long sum = 0;
+            foreach (var item in input)
+            {
+                sum += int.Parse(item);
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
